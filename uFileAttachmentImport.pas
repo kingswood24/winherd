cc4d418ca6d21b076a6f4c92e82c3a4f2459a579
MichaelKinsella <michael@kingswood.ie>
2021-02-25 16:28:25 +0000

    Update uFileAttachmentImport.pas

--- uFileAttachmentImport.pas
+++ uFileAttachmentImport.pas
@@ -8,9 +8,11 @@
    04/08/17 [V5.7 R1.1] /MK Change - SetupListView changed of Name column if import type is not ICBF.
 
    28/09/17 [V5.7 R3.3] /MK Additional Feature - Added Mark File As Read button that will prompt the user yes/no to mark as read. If yes then
-                                                 the usual process to mark a file as read is taken.   
+                                                 the usual process to mark a file as read is taken.
 
    05/10/17 [V5.7 R3.5] /MK Change - MarkAttachmentAsRead - If read file already exists with AttachmentName then add # and a number to AttachmentName.
+
+   24/02/21 [V5.9 R8.6] /MK Change - BuildFileList - Used a MemDataSet to allows sort by the date in descending order - Mary Brady request. 
 }
 
 unit uFileAttachmentImport;
@@ -20,7 +22,8 @@ interface
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
   StdCtrls, cxButtons, cxEdit, cxLabel, cxControls, cxContainer, cxListBox,
-  cxCheckBox, cxListView, ExtCtrls, ActnList, GenTypesConst, uHerdLookup;
+  cxCheckBox, cxListView, ExtCtrls, ActnList, GenTypesConst, uHerdLookup,
+  dxmdaset, cxUtils, db;
 
 type
   TImportType = (itICBF, itMart, itFactorySales, itVetLink, itCows365);
@@ -78,6 +81,7 @@ begin
    with TfmFileAttachmentImport.Create(nil) do
       try
          Result := '';
+         FFileCount := 0;
          FImportType := AImportType;
          FHerdIdentity := AHerdIdentity;
          BuildFileList;
@@ -88,10 +92,8 @@ begin
             Result := '';
       finally
          if ModalResult = mrOK then
-            begin
-               if cxListView1.Selected <> nil then
-                  Result := string( cxListView1.Selected.Data);
-            end;
+            if cxListView1.Selected <> nil then
+               Result := ( PString(cxListView1.Selected.Data)^ );
          Free;
       end;
 end;
@@ -106,7 +108,27 @@ var
    Directory : string;
    FileLocation : string;
    ListItem: TListItem;
-   FileCreationDate : TDateTime;
+   FileCreationDate,
+   FileModifiedDate : TDateTime;
+   MemData : TdxMemData;
+   ListItems : TStringList;
+
+   procedure AddItemToListView(const Text, DataText: string; ASubItems : TStrings);
+   var
+      pStr: PString;
+      i : Integer;
+   begin
+      { Allocate memory to hold our dynamic string data }
+      New(pStr);
+      pStr^ := DataText;
+      with cxListView1.Items.Add do begin
+        Caption := Text;
+        Data := pStr;
+        for i := 0 to ASubItems.Count-1 do
+           SubItems.Add(ASubItems[i]);
+      end;
+   end;
+
 begin
    SetupListView;
 
@@ -126,164 +148,130 @@ begin
                FFileList.Objects[i] := Pointer(0);
          end;
 
-   if ( FImportType = itICBF ) then
-      begin
-         //  format the remaining icbf file names
-         Caption := 'No of outstanding ICBF Files to import';
-         cxLabel1.Caption := Caption;
-         FFileCount := 0;
-         for i := 0 to FFileList.Count-1 do
+   case FImportType of
+      itICBF : Caption := 'No of outstanding ICBF Files to import';
+      itMart : Caption := 'No of outstanding Mart Files to import';
+      itFactorySales : Caption := 'No of outstanding Factory Files to import';
+      itVetLink : Caption := 'No of outstanding VetLink Files to import';
+      itCows365 : Caption := 'No of outstanding Cows365 Files to import';
+   end;
+   cxLabel1.Caption := Caption;
+
+   MemData := TdxMemData.Create(nil);
+   with MemData do
+      try
+         ClearMemDataFieldDefs(MemData);
+
+         CreateMemDataFieldDef(MemData,'FileCaption',ftString,1000);
+         CreateMemDataFieldDef(MemData,'FileName',ftString,1000);
+         CreateMemDataFieldDef(MemData,'FileDate',ftDateTime);
+         CreateMemDataFieldDef(MemData,'FileType',ftString,10);
+
+         MemData.SortedField := 'FileDate';
+         MemData.SortOptions := [soDesc];
+
+         MemData.Open;
+         MemData.First;
+
+         if ( FImportType = itICBF ) then
             begin
-               // remove any files previously read.
-               if ( not(cbIncludeAllFiles.Checked) ) then
+               //  format the remaining icbf file names
+               for i := 0 to FFileList.Count-1 do
                   begin
-                     if Integer(FFileList.Objects[i]) <> 0 then
-                        Continue;
+                     // remove any files previously read.
+                     if ( not(cbIncludeAllFiles.Checked) ) then
+                        begin
+                           if Integer(FFileList.Objects[i]) <> 0 then
+                              Continue;
+                        end;
+
+                     FileName := ExtractFileName(FFileList.Strings[i]);
+                     if ( Pos(cTagPrefix_IE, FFileList.Strings[i]) > 0 ) then
+                        HerdIdentifier := Copy(FileName, 3, 7)
+                     else if ( Pos(cTagPrefix_372, FFileList.Strings[i]) > 0 ) then
+                        HerdIdentifier := Copy(FileName, 4, 7)
+                     else if ( Pos(FHerdIdentity, FFileList.Strings[i]) > 0 ) then
+                        HerdIdentifier := FHerdIdentity;
+
+                     if ( Length(HerdIdentifier) > 0 ) then
+                        try
+                           if Pos('_read', FileName) > 0 then
+                              System.Delete(FileName, Pos('_read', FileName), 5);
+                           if ( not(HerdIdentifier = FHerdIdentity) ) then
+                              System.Insert(' ', HerdIdentifier, 3);
+                           FileType := Copy(FileName, Length(FileName)-4 , 1);
+                           if FileType = 'U' then
+                              FileType := 'Update'
+                           else
+                              FileType := 'New';
+
+                           FileDate := Copy(FileName, Length(FileName)-12, 8);
+                           D := StrToInt(Copy(FileDate, 1, 2));
+                           M := StrToInt(Copy(FileDate, 3, 2));
+                           Y := StrToInt(Copy(FileDate, 5, 4));
+                           FileCreationDate := EncodeDate(Y,M,D);
+
+                           MemData.Append;
+                           MemData.FieldByName('FileCaption').AsString := HerdIdentifier;
+                           MemData.FieldByName('FileName').AsString := FFileList.Strings[i];
+                           MemData.FieldByName('FileType').AsString := FileType;
+                           MemData.FieldByName('FileDate').AsDateTime := FileCreationDate;
+                           MemData.Post;
+                        except
+                        end;
                   end;
+            end
+         else
+            begin
+               for i := 0 to FFileList.Count-1 do
+                  begin
+                     // remove any files previously read.
+                     if not cbIncludeAllFiles.Checked then
+                        begin
+                           if Integer(FFileList.Objects[i]) <> 0 then
+                              Continue;
+                        end;
 
-               FileName := ExtractFileName(FFileList.Strings[i]);
-               if ( Pos(cTagPrefix_IE, FFileList.Strings[i]) > 0 ) then
-                  HerdIdentifier := Copy(FileName, 3, 7)
-               else if ( Pos(cTagPrefix_372, FFileList.Strings[i]) > 0 ) then
-                  HerdIdentifier := Copy(FileName, 4, 7)
-               else if ( Pos(FHerdIdentity, FFileList.Strings[i]) > 0 ) then
-                  HerdIdentifier := FHerdIdentity;
+                     FileName := ExtractFileName(FFileList.Strings[i]);
 
-               if ( Length(HerdIdentifier) > 0 ) then
-                  try
-                     if Pos('_read', FileName) > 0 then
-                        Delete(FileName, Pos('_read', FileName), 5);
-                     if ( not(HerdIdentifier = FHerdIdentity) ) then
-                        Insert(' ', HerdIdentifier, 3);
-                     FileType := Copy(FileName, Length(FileName)-4 , 1);
-                     if FileType = 'U' then
-                        FileType := 'Update'
-                     else
-                        FileType := 'New';
-                     FileDate := Copy(FileName, Length(FileName)-12, 8);
-                     D := StrToInt(Copy(FileDate, 1, 2));
-                     M := StrToInt(Copy(FileDate, 3, 2));
-                     Y := StrToInt(Copy(FileDate, 5, 4));
-                     ListItem := cxListView1.Items.Add;
-                     ListItem.Caption := HerdIdentifier;
-                     ListItem.Data := pointer(FFileList.Strings[i]);
-                     ListItem.SubItems.Add(FormatDateTime('dd of mmm yyyy', EncodeDate(Y,M,D)));
-                     ListItem.SubItems.Add(FileType);
-                     Inc(FFileCount);
-                  except
+                     if ( Pos('_read', FileName) > 0 ) then
+                        System.Delete(FileName, Pos('_read', FileName), 5);
+
+                     FileModifiedDate := GetFileModifiedDate(FFileList.Strings[i]);
+                     MemData.Append;
+                     MemData.FieldByName('FileCaption').AsString := FileName;
+                     MemData.FieldByName('FileName').AsString := FFileList.Strings[i];
+                     MemData.FieldByName('FileDate').AsDateTime := FileModifiedDate;
+                     MemData.Post;
                   end;
-
             end;
-      end
-   else if ( FImportType = itMart ) then
-      begin
-         Caption := 'No of outstanding Mart Files to import';
-         cxLabel1.Caption := Caption;
-         FFileCount := 0;
-         for i := 0 to FFileList.Count-1 do
+
+      cxListView1.Items.Clear;
+      ListItems := TStringList.Create();
+      try
+         MemData.First;
+         while ( not(MemData.Eof) ) do
             begin
-               // remove any files previously read.
-               if not cbIncludeAllFiles.Checked then
-                  begin
-                     if Integer(FFileList.Objects[i]) <> 0 then
-                        Continue;
-                  end;
-
-               FileName := ExtractFileName(FFileList.Strings[i]);
-
-               if ( Pos('_read', FileName) > 0 ) then
-                  Delete(FileName, Pos('_read', FileName), 5);
-
-               ListItem := cxListView1.Items.Add;
-               ListItem.Caption := FileName;
-               ListItem.Data := pointer(FFileList.Strings[i]);
-               FileCreationDate := GetFileCreationDate(FFileList.Strings[i]);
-               if ( FileCreationDate > 0 ) then
-                  ListItem.SubItems.Add(FormatDateTime('dd/mm/yyyy hh:mm', FileCreationDate));
+               if ( FImportType = itICBF ) then
+                  ListItems.Add(FormatDateTime('dd of mmm yyyy', MemData.FieldByName('FileDate').AsDateTime))
+               else
+                  ListItems.Add(FormatDateTime('dd/MM/yyyy', MemData.FieldByName('FileDate').AsDateTime));
+               ListItems.Add(MemData.FieldByName('FileType').AsString);
+               AddItemToListView(MemData.FieldByName('FileCaption').AsString,
+                                 MemData.FieldByName('FileName').AsString,
+                                 ListItems);
+               ListItems.Clear;
                Inc(FFileCount);
+               MemData.Next;
             end;
-      end
-   else if ( FImportType = itFactorySales ) then
-      begin
-         Caption := 'No of outstanding Factory Files to import';
-         cxLabel1.Caption := Caption;
-         FFileCount := 0;
-         for i := 0 to FFileList.Count-1 do
-            begin
-               // remove any files previously read.
-               if not cbIncludeAllFiles.Checked then
-                  begin
-                     if Integer(FFileList.Objects[i]) <> 0 then
-                        Continue;
-                  end;
-
-               FileName := ExtractFileName(FFileList.Strings[i]);
-               if ( Pos('_read', FileName) > 0 ) then
-                  Delete(FileName, Pos('_read', FileName), 5);
-
-               ListItem := cxListView1.Items.Add;
-               ListItem.Caption := FileName;
-               ListItem.Data := pointer(FFileList.Strings[i]);
-               FileCreationDate := GetFileCreationDate(FFileList.Strings[i]);
-               if ( FileCreationDate > 0 ) then
-                  ListItem.SubItems.Add(FormatDateTime('dd/mm/yyyy hh:mm', FileCreationDate));
-               Inc(FFileCount);
-            end;
-      end
-   else if ( FImportType = itVetLink ) then
-      begin
-         Caption := 'No of outstanding VetLink Files to import';
-         cxLabel1.Caption := Caption;
-         FFileCount := 0;
-         for i := 0 to FFileList.Count-1 do
-            begin
-               // remove any files previously read.
-               if not cbIncludeAllFiles.Checked then
-                  begin
-                     if Integer(FFileList.Objects[i]) <> 0 then
-                        Continue;
-                  end;
-
-               FileName := ExtractFileName(FFileList.Strings[i]);
-               if ( Pos('_read', FileName) > 0 ) then
-                  Delete(FileName, Pos('_read', FileName), 5);
-
-               ListItem := cxListView1.Items.Add;
-               ListItem.Caption := FileName;
-               ListItem.Data := pointer(FFileList.Strings[i]);
-               FileCreationDate := GetFileCreationDate(FFileList.Strings[i]);
-               if ( FileCreationDate > 0 ) then
-                  ListItem.SubItems.Add(FormatDateTime('dd/mm/yyyy hh:mm', FileCreationDate));
-               Inc(FFileCount);
-            end;
-      end
-   else if ( FImportType = itCows365 ) then
-      begin
-         Caption := 'No of outstanding Cows365 Files to import';
-         cxLabel1.Caption := Caption;
-         FFileCount := 0;
-         for i := 0 to FFileList.Count-1 do
-            begin
-               // remove any files previously read.
-               if not cbIncludeAllFiles.Checked then
-                  begin
-                     if Integer(FFileList.Objects[i]) <> 0 then
-                        Continue;
-                  end;
-
-               FileName := ExtractFileName(FFileList.Strings[i]);
-               if ( Pos('_read', FileName) > 0 ) then
-                  Delete(FileName, Pos('_read', FileName), 5);
-
-               ListItem := cxListView1.Items.Add;
-               ListItem.Caption := FileName;
-               ListItem.Data := pointer(FFileList.Strings[i]);
-               FileCreationDate := GetFileCreationDate(FFileList.Strings[i]);
-               if ( FileCreationDate > 0 ) then
-                  ListItem.SubItems.Add(FormatDateTime('dd/mm/yyyy hh:mm', FileCreationDate));
-               Inc(FFileCount);
-            end;
+      finally
+         if ( ListItems <> nil ) then
+            FreeAndNil(ListItems);
       end;
+   finally
+      MemData.Active := False;
+      FreeAndNil(MemData);
+   end;
 
    btnDelete.Visible := ( FFileCount > 0 ) and ( not(cbIncludeAllFiles.Checked) );
 end;
@@ -296,6 +284,7 @@ end;
 procedure TfmFileAttachmentImport.SetupListView;
 var
    ListColumn : TListColumn;
+   i : Integer;
 begin
    cxListView1.Items.Clear;
    cxListView1.Columns.Clear;
@@ -317,7 +306,7 @@ begin
          ListColumn.Caption := 'Name';
          ListColumn.Width := 300;
          ListColumn := cxListView1.Columns.Add;
-         ListColumn.Caption := 'Date created';
+         ListColumn.Caption := 'Date modified';
          ListColumn.Width := 120;
       end;
 end;
@@ -393,7 +382,7 @@ procedure TfmFileAttachmentImport.actMarkFileAsReadExecute(Sender: TObject);
 begin
    if ( MessageDlg('Are you sure you want to mark this file as read?',mtConfirmation,[mbYes,mbNo],0) = mrYes ) then
       begin
-         MarkAttachmentAsRead(string(cxListView1.Selected.Data));
+         MarkAttachmentAsRead(PString(cxListView1.Selected.Data)^);
          BuildFileList;
       end;
 end;
